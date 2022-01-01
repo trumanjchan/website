a62f6b6cd116f6f2f0e6093290cdf3852afb1f85
Truman <trumanjfchan@gmail.com>
2022-01-01 15:03:27 -0800

    Added Toggle keeping mode state after page refresh
    Before these changes, if I refreshed the page with dark mode on, the page would default back to light mode.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,8 +3,31 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
-    function toggleMode() {
-        document.querySelector("body").classList.toggle("dark-mode");
+    // Finds whether pre-reload 'data-theme' was light or dark, then renders that theme.
+    const x = localStorage.getItem("data-theme");
+    console.log(x);
+    if (x === "light") {
+        document.documentElement.setAttribute("data-theme", "light");
+    }
+    else {
+        document.documentElement.setAttribute("data-theme", "dark");
+    }
+
+    const toggleMode = (e) => {
+        /* Switch to Light Mode */
+        if (e.currentTarget.checked === false) {
+            // Sets the custom HTML attribute
+            document.documentElement.setAttribute("data-theme", "light");
+            // Sets the user's preference in local storage
+            localStorage.setItem("data-theme", "light");
+        }
+        else {
+            /* Switch to Dark Mode */
+            // Sets the custom HTML attribute
+            document.documentElement.setAttribute("data-theme", "dark");
+            // Sets the user's preference in local storage
+            localStorage.setItem("data-theme", "dark");
+        }
     }
 
     return (
@@ -12,7 +35,12 @@ function Navbar() {
             <ul className='grid-container'>
                 <li><Link to="/">About</Link></li>
                 <li><Link to="/home">Home</Link></li>
-                <li><button onClick={toggleMode}>Light/Dark</button></li>
+                <li>
+                    <label className="switch">
+                        <input type="checkbox" onClick={toggleMode} />
+                        <div></div>
+                    </label>
+                </li>
             </ul>
         </div>
     );
