b17fe95c3c1a3d282bfcf4e579faeb1dca555deb
Truman <trumanjfchan@gmail.com>
2022-01-01 23:00:26 -0800

    Test fix on '/home' again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import About from '../src/Pages/About/About';
 import Home from '../src/Pages/Home/Home';
@@ -8,8 +8,8 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path='/' element={<About/>} />
-        <Route path='/gh-pages-url/home' element={<Home/>} />
+        <Route path='/*' element={<About/>} />
+        <Route path='/home' element={<Home/>} />
       </Routes>
     </Router>
   );
