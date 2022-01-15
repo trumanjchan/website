3ee570b38d8579a38888daed269f921a3fb14aab
Truman <trumanjfchan@gmail.com>
2022-01-14 18:02:20 -0800

    Added import Navigate to redirect to 404 page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import About from '../src/Pages/About/About';
 import Home from '../src/Pages/Home/Home';
@@ -11,11 +11,12 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path='*' element={<PageNotFound/>} />
         <Route path='/' element={<About/>} />
         <Route path='/home' element={<Home/>} />
         <Route path='/guides' element={<Guides/>} />
         <Route path='/tasks' element={<Tasks/>} />
+        <Route path='/404' element={<PageNotFound/>} />
+        <Route path='*' element={<Navigate replace to='/404'/>} />
       </Routes>
     </Router>
   );
