e0f4c7a44698eccea05a61470bcd7e16c7c9b975
Truman <trumanjfchan@gmail.com>
2022-01-17 18:04:51 -0800

    Added 404 page redirect back
    Because a test fixed all the endpoints giving github 404 page when refreshed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import About from '../src/Pages/About/About';
 import Home from '../src/Pages/Home/Home';
 import Guides from '../src/Pages/Guides/Guides';
 import Tasks from './Pages/Tasks/Tasks';
+import PageNotFound from './Pages/PageNotFound/PageNotFound';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path='/home' element={<Home/>} />
         <Route path='/guides' element={<Guides/>} />
         <Route path='/tasks' element={<Tasks/>} />
+        <Route path='/404' element={<PageNotFound/>} />
+        <Route path='*' element={<Navigate replace to='/404'/>} />
       </Routes>
     </Router>
   );
